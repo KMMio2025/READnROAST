69f245e357255b163369e9f07ddbd473fdcab757
karo <karolinykulas@gmail.com>
2025-04-18 21:54:52 +0200

    navbar component added

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="nav-brand">
+        <img src="/images/logo.png" alt="Logo" className="appLogo" />
+      </div>
+      
+      <div className="nav-left">
+
+        <a href="#" className="nav-link">Home</a>
+        <i className='bx bxs-coffee-bean bean'></i>
+        <a href="#" className="nav-link">About</a>
+        <i className='bx bxs-coffee-bean bean'></i>
+        <a href="#" className="nav-link">Services</a>
+        <i className='bx bxs-coffee-bean bean'></i>
+        <a href="#" className="nav-link">Contact</a>
+      </div>
+      
+      <div className="nav-right">
+        <a href="#" className="nav-link login-btn">Login</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
